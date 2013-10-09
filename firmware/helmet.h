d0c8df6904b3f90e5282ecca1932e25fe8e9f945
Roman Moravčík <roman.moravcik@gmail.com>
2013-10-09 15:57:12 +0200

    Added comments to code.

--- firmware/helmet.h
+++ firmware/helmet.h
@@ -10,6 +10,7 @@ void helmet_init(void);
 
 void helmet_open(void);
 void helmet_close(void);
+
 uint8_t helmet_state(void);
 
 #endif // HELMET_H
--- firmware/repulsor.c
+++ firmware/repulsor.c
@@ -8,13 +8,16 @@
 
 void repulsor_init(void)
 {
+	// set left/right repuslor pins as an output
 	DDRD |= _BV(GPIO_LEFT_REPULSOR) | _BV(GPIO_RIGHT_REPULSOR);
-	DDRD &= ~_BV(GPIO_SHOT_REPULSOR);
+
+	// set left/right repulsor pins to low
 	PORTD &= ~(_BV(GPIO_LEFT_REPULSOR) | _BV(GPIO_RIGHT_REPULSOR));
 }
 
 void repulsor_shot(void)
 {
+	// simulate repulsor blast
 	PORTD |= _BV(GPIO_LEFT_REPULSOR) | _BV(GPIO_RIGHT_REPULSOR);
 	_delay_ms(200);
 	PORTD &= ~(_BV(GPIO_LEFT_REPULSOR) | _BV(GPIO_RIGHT_REPULSOR));
