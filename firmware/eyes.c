8ab51dca3cc423ab626908a92144faf5fa54b607
Roman Moravčík <roman.moravcik@gmail.com>
2013-10-11 15:54:12 +0200

    Removed effects.

--- firmware/eyes.c
+++ firmware/eyes.c
@@ -20,23 +20,34 @@
 #include <util/delay.h>
 
 #include "common.h"
-#include "effects.h"
 #include "eyes.h"
+
+enum {
+	FADE_IN = 0,
+	FADE_OUT
+};
+
+static void eyes_blink(void);
+static void eyes_fade_effect(uint8_t mode);
 
 void eyes_init()
 {
-	effect_init(GPIO_EYES);
+	// set eyes pin as in output
+	DDRB |= _BV(GPIO_EYES);
+
+	// set eyes pin to low
+	PORTB &= ~_BV(GPIO_EYES);
 }
 
 void eyes_power_up(void)
 {
-	effect_set_mode(GPIO_EYES, MODE_BLINK);
-	effect_set_mode(GPIO_EYES, MODE_FADE_IN);
+	eyes_blink();
+	eyes_fade_effect(FADE_IN);
 }
 
 void eyes_power_down(void)
 {
-	effect_set_mode(GPIO_EYES, MODE_OFF);
+	PORTB &= ~_BV(GPIO_EYES);
 
 	// wait for 200ms before opening helmet
 	_delay_ms(200);
@@ -44,5 +55,60 @@ void eyes_power_down(void)
 
 void eyes_power_failure(void)
 {
-	effect_set_mode(GPIO_EYES, MODE_BLINK);
+	eyes_blink();
+}
+
+// takes 200ms
+static void eyes_blink(void)
+{
+	PORTB &= ~_BV(GPIO_EYES);
+	_delay_ms(50);
+	PORTB |= _BV(GPIO_EYES);
+	_delay_ms(50);
+	PORTB &= ~_BV(GPIO_EYES);
+	_delay_ms(50);
+	PORTB |= _BV(GPIO_EYES);
+	_delay_ms(50);
+}
+
+// takes 3000ms
+static void eyes_fade_effect(uint8_t mode)
+{
+	uint8_t step, i = 0;
+	uint8_t time, off_time, on_time;
+
+	// do fade effect in 50 steps
+	for (step = 0; step <= 50; step++) {
+		// calculate on/off times for PWM with period 1kHz
+		if (mode == FADE_IN) {
+			on_time = 2 * step;
+			off_time = 100 - on_time;
+		} else {
+			off_time = 2 * step;
+			on_time = 100 - off_time;
+		}
+
+		// each PWM step is taking 60ms
+		for (i = 0; i < 30; i++) {
+			PORTB &= ~_BV(GPIO_EYES);
+
+			for (time = 0; time < off_time; time++) {
+				_delay_us(20);
+			}
+
+			PORTB |= _BV(GPIO_EYES);
+
+			for (time = 0; time < on_time; time++) {
+				_delay_us(20);
+			}
+		}
+	}
+
+	// if fade in, leave gpio asserted high
+	// else set it low
+	if (mode == FADE_IN) {
+		PORTB |= _BV(GPIO_EYES);
+	} else {
+		PORTB &= ~_BV(GPIO_EYES);
+	}
 }
--- firmware/repulsor.c
+++ firmware/repulsor.c
@@ -20,40 +20,92 @@
 #include <util/delay.h>
 
 #include "common.h"
-#include "effects.h"
 #include "repulsor.h"
+
+enum {
+	FADE_IN = 0,
+	FADE_OUT
+};
+
+static void repulsor_fade_effect(uint8_t mode);
 
 void repulsor_init(void)
 {
-	effect_init(GPIO_REPULSOR_PWR);
+	// set repuslor blast and pwr pins as an output
+	DDRD |= _BV(GPIO_REPULSOR_BLAST) | _BV(GPIO_REPULSOR_PWR);
 
-	// set repuslor blast pins as an output
-	DDRD |= _BV(GPIO_REPULSOR_BLAST);
-
-	// set repulsor blast pins to low
-	PORTD &= ~_BV(GPIO_REPULSOR_BLAST);
+	// set repulsor blast and pwr pins to low
+	PORTD &= ~(_BV(GPIO_REPULSOR_BLAST) | _BV(GPIO_REPULSOR_PWR));
 }
 
 void repulsor_power_up(void)
 {
-	effect_set_mode(GPIO_REPULSOR_PWR, MODE_FADE_IN);
+	repulsor_fade_effect(FADE_IN);
 }
 
 void repulsor_power_down(void)
 {
-	effect_set_mode(GPIO_REPULSOR_PWR, MODE_FADE_OUT);
+	repulsor_fade_effect(FADE_OUT);
 }
 
 void repulsor_power_failure(void)
 {
-	effect_set_mode(GPIO_REPULSOR_PWR, MODE_BLINK);
+	PORTD &= ~_BV(GPIO_REPULSOR_PWR);
+	_delay_ms(50);
+	PORTD |= _BV(GPIO_REPULSOR_PWR);
+	_delay_ms(50);
+	PORTD &= ~_BV(GPIO_REPULSOR_PWR);
+	_delay_ms(50);
+	PORTD |= _BV(GPIO_REPULSOR_PWR);
+	_delay_ms(50);
 }
 
 void repulsor_blast(void)
 {
 	// simulate repulsor blast
 	PORTD |= _BV(GPIO_REPULSOR_BLAST);
-	_delay_ms(200);
+	_delay_ms(100);
 	PORTD &= ~_BV(GPIO_REPULSOR_BLAST);
 }
 
+// takes 3000ms
+static void repulsor_fade_effect(uint8_t mode)
+{
+	uint8_t step, i = 0;
+	uint8_t time, off_time, on_time;
+
+	// do fade effect in 50 steps
+	for (step = 0; step <= 50; step++) {
+		// calculate on/off times for PWM with period 1kHz
+		if (mode == FADE_IN) {
+			on_time = 2 * step;
+			off_time = 100 - on_time;
+		} else {
+			off_time = 2 * step;
+			on_time = 100 - off_time;
+		}
+
+		// each PWM step is taking 60ms
+		for (i = 0; i < 30; i++) {
+			PORTD &= ~_BV(GPIO_REPULSOR_PWR);
+
+			for (time = 0; time < off_time; time++) {
+				_delay_us(20);
+			}
+
+			PORTD |= _BV(GPIO_REPULSOR_PWR);
+
+			for (time = 0; time < on_time; time++) {
+				_delay_us(20);
+			}
+		}
+	}
+
+	// if fade in, leave gpio asserted high
+	// else set it low
+	if (mode == FADE_IN) {
+		PORTD |= _BV(GPIO_REPULSOR_PWR);
+	} else {
+		PORTD &= ~_BV(GPIO_REPULSOR_PWR);
+	}
+}
